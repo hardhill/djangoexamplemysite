7a0c376e417b47dc81e2a957e592ace0ec23a45c
hardhill <hardhill@mail.ru>
2017-03-27 01:16:05 +0800

    Changed the HTML for the site.

--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.utils import timezone
+
+from mysite.models import Post
+
+
 def post_list(request):
-    text = "Hello user"
-    context = {'title':text}
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    context = {'posts':posts}
     return render(request,'mysite/index.html',context)
